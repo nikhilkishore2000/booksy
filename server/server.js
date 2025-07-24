1324a3ce7f0a937e2af1b9873fd95000ae656aa6
Nikhil Kishore <nuth.nik@gmail.com>
2025-07-25 00:19:07 +0530

    configured the expression session

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const authRoutes = require("./routes/auth.routes");
 const path = require("path");
 const livereload = require("livereload");
 const connectLivereload = require("connect-livereload");
+const session = require("express-session");
+const defaultSession = require("./utils/session");
 
 //env load
 dotenv.config();
@@ -30,10 +32,33 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Session middleware
+app.use(
+  session({
+    secret: "your-random-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// 👇 Initialize default session object if missing
+app.use((req, res, next) => {
+  if (!req.session.sessionData) {
+    req.session.sessionData = JSON.parse(JSON.stringify(defaultSession));
+  }
+  next();
+});
+
+// 👇 Make session data available to all EJS views
+app.use((req, res, next) => {
+  res.locals.session = req.session.sessionData;
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.render("signup", { title: "Welcome to My App" });
+  res.render("signup", { session: req.session.sessionData.defaultSession });
 });
 
 //Notify browser on change
---
+++ server/utils/session.js
@@ -0,0 +1,8 @@
+const defaultSession = {
+  user: {
+    role: "user",
+    username: "",
+  },
+};
+
+module.exports = { defaultSession };
